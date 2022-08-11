cdce9210646fa0f67bbe572fdfa0a6fca92ca05a
Leah <zleahbz@gmail.com>
2022-08-11 19:59:27 +0100

    Update author

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,7 +3,7 @@ Creation Date:	25/06/2021
 ]]------------------------------------
 fx_version 'cerulean'
 game 'gta5'
-author 'Leah#0001'
+author 'leah_uk'
 version '1.1.1'
 versioncheck 'https://raw.githubusercontent.com/Leah-UK/bixbi_npcsales/main/fxmanifest.lua'
 lua54 'yes'
